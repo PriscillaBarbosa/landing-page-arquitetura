7ef60a0df3bf274626b21e9c607c688e827388c0
Priscilla Barbosa <priscillabarbosa@outlook.com>
2025-03-09 22:27:24 -0300

    ajustes css

--- form-handler.php
+++ form-handler.php
@@ -5,21 +5,12 @@ if($_SERVER[REQUEST_METHOD] == "POST") {
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
     $mensagem = htmlspecialchars($_POST['mensagem']);
 
-    //validação simples dos dados do formulário
     if (!empty($nome) && !empty($email) && !empty($mensagem)) {
-        //destinatário do e-mail
         $to = "priscillabarbosa@outlook.com"; 
-
-        //assunto do email
         $subject = "Novo contato de $nome";
-
-        //corpo do email
         $body = "Nome: $nome\nEmail: $email\n\nMensagem:\n$mensagem";
-
-        //cabeçalhos do e-mail
         $headers = "From: $email";
 
-        //envio do email
         if (mail($to, $subject, $body, $headers)) {
             echo "Mensagem enviada com sucesso!";
         } else {
